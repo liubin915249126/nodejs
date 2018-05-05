c2a7f1ea93e9f717da165a3186e5084946391ae4
刘宾 <915249126@qq.com>
2018-05-05 18:31:28 +0800

    add pass

--- googleplay/email.js
+++ googleplay/email.js
@@ -36,7 +36,7 @@ function postEmail(datas){
             transporter.sendMail(mailOptions, (error, info) => {
                 if (error) {
                     return console.log(error);
-                }
+                }   
                 console.log('Message sent: %s', info.messageId);
                 // Preview only available when sending through an Ethereal account
                 console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
